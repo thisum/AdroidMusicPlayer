e311a7348afe6e60b39ec83a1f5a3e257cffc4b6
Thisum <thisum.kb@gmail.com>
2017-04-18 13:33:09 +0800

    first commit

---
+++ app/src/main/java/phn/tapio/ahlab/org/musicapp/util/MusicPlayerUtil.java
@@ -0,0 +1,18 @@
+package phn.tapio.ahlab.org.musicapp.util;
+
+/**
+ * Created by thisum on 3/27/2017.
+ */
+
+public class MusicPlayerUtil
+{
+    public static String getFormattedTime( int milliseconds )
+    {
+        String ret = "";
+        int seconds = ( milliseconds / 1000 ) % 60;
+        int minutes = ( ( milliseconds / ( 1000 * 60 ) ) % 60 );
+        ret += minutes < 10 ? "0" + minutes + ":" : minutes + ":";
+        ret += seconds < 10 ? "0" + seconds : seconds + "";
+        return ret;
+    }
+}
